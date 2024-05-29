1c8890ca8b5aefe5d5a0ebdd6697b0482d43810d
Omer Naveed <contact@mail.omernaveed.dev>
2024-05-29 03:36:02 -0500

    feat: add data to database
    Add required data to database. Data was scraped from Bulbapedia using
    the Python script in 'scripts/scrapeRecipeData.py'.
    
    The SQL tables were structured with a many-to-many relationship with an
    intermediate table to join the dishes and ingredients. There are
    currently a total of 16 rows in the 'Ingredient' table, 60 rows in the
    'Dish' table and 960 rows in the 'DishIngredient' table.
    
    A sample DB query was added to 'src/pages/index.astro' to demonstrate
    how data will be fetched from the database.

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,32 @@
-import { defineDb } from "astro:db";
+import { column, defineDb, defineTable } from "astro:db";
+
+const Ingredient = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text({ unique: true }),
+    strength: column.number(),
+  },
+});
+
+const Dish = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text({ unique: true }),
+    type: column.text(),
+    baseStrength: column.number(),
+    minPotSize: column.number(),
+  },
+});
+
+const DishIngredient = defineTable({
+  columns: {
+    dishId: column.number({ references: () => Dish.columns.id }),
+    ingredientId: column.number({ references: () => Ingredient.columns.id }),
+    quantity: column.number(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {},
+  tables: { Ingredient, Dish, DishIngredient },
 });

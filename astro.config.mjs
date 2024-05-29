1d2fdde8fb9bdd89825640b57a0ba7332cec2df8
Omer Naveed <contact@mail.omernaveed.dev>
2024-05-29 00:09:47 -0500

    fix: format existing files using prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 
 import vercel from "@astrojs/vercel/serverless";
@@ -7,5 +7,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [db()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
